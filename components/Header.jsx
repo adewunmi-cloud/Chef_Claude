96d0733c42effe1cd8e78a140e2bc7c44b1e8de7
adewunmi-cloud <ashonibare63@gmail.com>
2026-04-23 12:27:56 +0100

    add header.jsx

---
+++ components/Header.jsx
@@ -0,0 +1,8 @@
+export default function Header() {
+  return (
+    <nav className="header">
+      <img src="./src/assets/chef_logo.jpg" alt="chef_logo" className="logo" />
+      <h1>Chef Claude</h1>
+    </nav>
+  )
+}
