15859f0305dcc80777146b7bd02209995b35b4d3
adewunmi-cloud <ashonibare63@gmail.com>
2026-05-14 15:16:08 +0100

    add scroll function to main page

--- components/IngredientsList.jsx
+++ components/IngredientsList.jsx
@@ -1,11 +1,11 @@
-export default function IngredientsList({ingredients, getRecipe}) {
+export default function IngredientsList({ingredients, getRecipe, ref}) {
   const ingredientsList = ingredients.map(item => <li key={item}>{item}</li>)
   return (
     <section>
         <h2>Ingredients on hand:</h2>
           <ul className="ingredients-list" aria-live="polite">{ingredientsList}</ul>
           {ingredients.length >= 4 && <div className="get-recipe-container">
-          <div>
+          <div ref={ref}>
             <h3>Ready for a recipe?</h3>
             <p>Generate a recipe from your list of ingredients.</p>
           </div>
--- components/Main.jsx
+++ components/Main.jsx
@@ -1,4 +1,4 @@
-import { useState } from "react"
+import { useState, useRef, useEffect } from "react"
 import IngredientsList from "./IngredientsList"
 import ClaudeRecipe from "./ClaudeRecipe"
 import { getRecipeFromMistral } from "../ai"
@@ -6,7 +6,13 @@ import { getRecipeFromMistral } from "../ai"
 export default function Main () {
   const [ingredients, setIngredients] = useState([])
   const [recipe, setRecipe] = useState("")
+  const recipeSection = useRef(null)
   
+  useEffect(() => {
+    if (recipe !== "" && recipeSection.current !== null) {
+      recipeSection.current.scrollIntoView({behavior: "smooth"})
+    }
+  }, [recipe])
 
   function addIngredient(formData) {
     const newIngredient = formData.get("ingredient")
@@ -28,7 +34,7 @@ export default function Main () {
         name="ingredient" />
         <button>Add ingredient</button>
       </form>
-      {ingredients.length > 0 && <IngredientsList ingredients={ingredients} getRecipe={getRecipe}/>}
+      {ingredients.length > 0 && <IngredientsList ingredients={ingredients} getRecipe={getRecipe} ref={recipeSection}/>}
       {recipe && <ClaudeRecipe recipe={recipe} />}
     </main>
   )
